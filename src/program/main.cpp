4f2f227d23bf2165009ac178e7d7b90a406d0963
CraftyBoss <talibabdulmaalik@gmail.com>
2023-02-05 18:15:46 -0800

    add custom exception handler, remove useless code

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -29,9 +29,10 @@
 #include "agl/utl.h"
 #include "imgui_nvn.h"
 #include "helpers/InputHelper.h"
-#include "init.h"
 #include "helpers/PlayerHelper.h"
 #include "game/GameData/GameDataFunction.h"
+
+#include "ExceptionHandler.h"
 
 static const char *DBG_FONT_PATH = "DebugData/Font/nvn_font_jis1.ntx";
 static const char *DBG_SHADER_PATH = "DebugData/Font/nvn_font_shader_jis1.bin";
@@ -40,119 +41,6 @@ static const char *DBG_TBL_PATH = "DebugData/Font/nvn_font_jis1_tbl.bin";
 #define IMGUI_ENABLED true
 
 sead::TextWriter *gTextWriter;
-
-void drawBackground() {
-
-    agl::DrawContext *context = Application::instance()->mDrawInfo->mDrawContext;
-
-    sead::Vector3<float> p1(-1, .3, 0); // top left
-    sead::Vector3<float> p2(-.2, .3, 0); // top right
-    sead::Vector3<float> p3(-1, -1, 0); // bottom left
-    sead::Vector3<float> p4(-.2, -1, 0); // bottom right
-    sead::Color4f c(.1, .1, .1, .9);
-
-    agl::utl::DevTools::beginDrawImm(context, sead::Matrix34<float>::ident, sead::Matrix44<float>::ident);
-    agl::utl::DevTools::drawTriangleImm(context, p1, p2, p3, c);
-    agl::utl::DevTools::drawTriangleImm(context, p3, p4, p2, c);
-}
-
-void drawCursor() {
-
-    agl::DrawContext *context = Application::instance()->mDrawInfo->mDrawContext;
-
-    nn::hid::MouseState state{};
-    nn::hid::GetMouseState(&state);
-    agl::utl::DevTools::beginDrawImm(context, sead::Matrix34<float>::ident,
-                                     sead::Matrix44<float>::ident);
-    sead::Vector2f screenSize = sead::Vector2f(1280.0f, 720.0f) / 2.0f;
-    agl::utl::DevTools::drawCursor(context, screenSize, sead::Vector2f(((float) state.x - screenSize.x) / screenSize.x,
-                                                                       ((float) -state.y + screenSize.y) /
-                                                                       screenSize.y), 0.5f);
-
-}
-
-void drawImGuiDebug() {
-
-    auto bd = ImguiNvnBackend::getBackendData();
-    ImGuiIO io = ImGui::GetIO();
-
-    ImVec2 mousePos;
-    InputHelper::getMouseCoords(&mousePos.x, &mousePos.y);
-    ImVec2 scrollDelta;
-    InputHelper::getScrollDelta(&scrollDelta.x, &scrollDelta.y);
-
-    gTextWriter->setCursorFromTopLeft(sead::Vector2f(10.f, 480.f));
-
-    nn::TimeSpan curTick = nn::os::GetSystemTick().ToTimeSpan();
-    nn::TimeSpan prevTick(bd->lastTick);
-    float sec = fabsf((double) (curTick - prevTick).GetNanoSeconds() / 1000000000.0);
-
-    gTextWriter->printf("Delta Time: (u64) %llu (float) %f\n", sec, (float) sec);
-
-    gTextWriter->printf("ImGui Delta Time: %f\n", io.DeltaTime);
-
-    gTextWriter->printf("Mouse Coords: X: %f Y: %f\n", mousePos.x, mousePos.y);
-    gTextWriter->printf("Scroll Delta: X: %f Y: %f\n", scrollDelta.x, scrollDelta.y);
-}
-
-void graNoclipCode(al::LiveActor *player) {
-
-    static bool isFirst = true;
-
-    static float speed = 25.0f;
-    static float speedMax = 150.0f;
-    static float vspeed = 20.0f;
-    static float speedGain = 0.0f;
-
-    sead::Vector3f *playerPos = al::getTransPtr(player);
-    sead::Vector3f *cameraPos = al::getCameraPos(player, 0);
-    sead::Vector2f *leftStick = al::getLeftStick(-1);
-
-    // Its better to do this here because loading zones reset this.
-    al::offCollide(player);
-    al::setVelocityZero(player);
-
-    // Mario slightly goes down even when velocity is 0. This is a hacky fix for that.
-    playerPos->y += 1.4553f;
-
-    float d = sqrt(al::powerIn(playerPos->x - cameraPos->x, 2) + (al::powerIn(playerPos->z - cameraPos->z, 2)));
-    float vx = ((speed + speedGain) / d) * (playerPos->x - cameraPos->x);
-    float vz = ((speed + speedGain) / d) * (playerPos->z - cameraPos->z);
-
-    if (!al::isPadHoldZR(-1)) {
-        playerPos->x -= leftStick->x * vz;
-        playerPos->z += leftStick->x * vx;
-
-        playerPos->x += leftStick->y * vx;
-        playerPos->z += leftStick->y * vz;
-
-        if (al::isPadHoldX(-1)) speedGain -= 0.5f;
-        if (al::isPadHoldY(-1)) speedGain += 0.5f;
-        if (speedGain <= 0.0f) speedGain = 0.0f;
-        if (speedGain >= speedMax) speedGain = speedMax;
-
-        if (al::isPadHoldZL(-1) || al::isPadHoldA(-1)) playerPos->y -= (vspeed + speedGain / 6);
-        if (al::isPadHoldB(-1)) playerPos->y += (vspeed + speedGain / 6);
-    }
-}
-
-void controlLol(StageScene *scene) {
-    auto actor = rs::getPlayerActor(scene);
-
-    static bool isNoclip = false;
-
-    if (al::isPadTriggerRight(-1)) {
-        isNoclip = !isNoclip;
-
-        if (!isNoclip) {
-            al::onCollide(actor);
-        }
-    }
-
-    if (isNoclip) {
-        graNoclipCode(actor);
-    }
-}
 
 void drawDebugWindow() {
     al::Sequence *curSequence = GameSystemFunction::getGameSystem()->mCurSequence;
@@ -239,7 +127,6 @@ void drawDebugWindow() {
 
 HOOK_DEFINE_TRAMPOLINE(ControlHook) {
     static void Callback(StageScene *scene) {
-        controlLol(scene);
         Orig(scene);
     }
 };
@@ -372,16 +259,10 @@ HOOK_DEFINE_TRAMPOLINE(DrawDebugMenu) {
 
         Orig(thisPtr);
 
-        // drawBackground();
-
-//        drawCursor();
-
         gTextWriter->beginDraw();
 
         gTextWriter->setCursorFromTopLeft(sead::Vector2f(10.f, 10.f));
         gTextWriter->printf("FPS: %d\n", static_cast<int>(round(Application::instance()->mFramework->calcFps())));
-
-//        drawImGuiDebug();
 
         gTextWriter->endDraw();
 
@@ -391,6 +272,8 @@ HOOK_DEFINE_TRAMPOLINE(DrawDebugMenu) {
 extern "C" void exl_main(void *x0, void *x1) {
     /* Setup hooking enviroment. */
     exl::hook::Initialize();
+
+    nn::os::SetUserExceptionHandler(exception_handler, nullptr, 0, nullptr);
 
     R_ABORT_UNLESS(Logger::instance().init(LOGGER_IP, 3080).value);
 
