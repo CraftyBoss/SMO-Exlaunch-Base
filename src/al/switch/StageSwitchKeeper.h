2347426b7db61f70d8fae5cac8b67e29c5b51637
CraftyBoss <talibabdulmaalik@gmail.com>
2023-02-19 15:22:17 -0800

    update header (fixes live actor init stageswitch crash), remove abort on logger connection failure

--- src/al/switch/StageSwitchKeeper.h
+++ src/al/switch/StageSwitchKeeper.h
@@ -1,21 +1,32 @@
 #pragma once
 
 #include "al/actor/IUseName.h"
+#include "al/actor/Placement.h"
 
-namespace al
-{
+namespace al {
     class StageSwitchKeeper;
-    
-    class IUseStageSwitch : virtual public al::IUseName
-    {
+
+    class StageSwitchDirector;
+
+    class StageSwitchAccesser;
+
+    class IUseStageSwitch : virtual public al::IUseName {
     public:
-        virtual al::StageSwitchKeeper* getStageSwitchKeeper() const = 0;
+        virtual al::StageSwitchKeeper *getStageSwitchKeeper() const = 0;
+
         virtual void initStageSwitchKeeper() = 0;
     };
 
-    class StageSwitchKeeper
-    {
+    class StageSwitchKeeper {
     public:
         StageSwitchKeeper();
+
+        void init(al::StageSwitchDirector *, al::PlacementInfo const &);
+
+        void tryGetStageSwitchAccesser(char const *);
+
+        StageSwitchAccesser *mStageSwitchAccesser = nullptr; // 0x0
+        int mLinkCount = 0; // 0x8
+        const char *mSwitchName = nullptr; // 0x10
     };
 };
--- src/program/main.cpp
+++ src/program/main.cpp
@@ -275,7 +275,7 @@ extern "C" void exl_main(void *x0, void *x1) {
 
     nn::os::SetUserExceptionHandler(exception_handler, nullptr, 0, nullptr);
 
-    R_ABORT_UNLESS(Logger::instance().init(LOGGER_IP, 3080).value);
+    Logger::instance().init(LOGGER_IP, 3080);
 
     runCodePatches();
 
